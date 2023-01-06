7646aa9a4f55cd6777e018d3289616803b3df8d0
Salvador Perez <salvadorperezm115@gmail.com>
2023-01-06 17:29:25 -0600

    adding the command to run the tests and a few examples

---
+++ src/test/example-01.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function divider(a, b) {
+  return a / b;
+}
+module.exports = sum;
+module.exports = divider;
---
+++ src/test/example-01.test.js
@@ -0,0 +1,17 @@
+// const sum = require("./example-01");
+// const divider = require("./example-01");
+
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sum(1, 2)).toBe(3); // this will pass
+//   expect(sum(2, 2).toBe(5)); // this will fail
+// });
+
+// test("division of 2 / 2", () => {
+//   expect(divider(2, 2)).toBe(1); // this will pass
+//   expect(divider(2, 0)).toBe(0); // this will fail
+// });
+
+// test("testing null values", () => {
+//   const value = null;
+//   expect(value).toBeNull(); // this will pass
+// });
